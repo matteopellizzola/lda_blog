bad17c60d48fefb5d466d3d0d2aaaf055bcd2424
matteopellizzola <57703690+matteopellizzola@users.noreply.github.com>
2022-12-23 22:56:48 +0100

    first skeleton React DOM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Route, Routes, Outlet} from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<h1>Home Page</h1>}></Route>
+                    <Route path="/about" element={ <h1>About Page</h1>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+function Layout (props) {
+    return <>
+        <h1>Website title</h1>
+
+        <Outlet />
+
+        footer?
+    </>;
 }
 
 export default App;
