0a62082f5bcfa9c71d69b99402d7ddcb6cea7b07
matteopellizzola <57703690+matteopellizzola@users.noreply.github.com>
2022-12-27 23:22:13 +0100

    Icone e stile menu mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,20 @@ import { Col, Row } from "react-bootstrap";
 
 import "./header.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header (props) {
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    function toggleMobileMenu(){
+        if (mobileMenu) {
+            setMobileMenu(false);
+        } else {
+            setMobileMenu(true);
+        }
+    }
+
     return <>
         <Row className="menu-header-desktop">
             {/* <div className="hamb-menu d-none">=</div> */}
@@ -31,7 +43,53 @@ function Header (props) {
                     </Link>
                 </div>
             </Col>
-      </Row>
+        </Row>
+        <Row className="menu-header-mobile">
+            <Col>
+                <div className="menu-wrapper">
+                    <div className="menu-link" onClick={() => toggleMobileMenu()}>
+                        <h1>
+                            <i className="icon-menu"></i>
+                        </h1>
+                    </div>
+                    <div className="mobile-logo">
+                        <h1>
+                            <Link to="/">
+                                <i className="icon-home"></i>
+                            </Link>
+                        </h1>
+                    </div>
+                </div>
+            </Col>
+            {mobileMenu && <ModalMobileMenu toggleMobileMenu={toggleMobileMenu} /> }
+        </Row>
+    </>;
+}
+
+function ModalMobileMenu (props) {
+
+    return <>
+        <Col className="modal-menu-mobile">
+            <div className="link-wrapper">
+                <div className="menu-link">
+                    <Link to="/about" onClick={() => props.toggleMobileMenu()}>
+                        <div className="menu-text">Chi Sono</div>
+                    </Link>
+                </div>
+
+                <div className="menu-link">
+                    <Link to="/contacts" onClick={() => props.toggleMobileMenu()}>
+                        <div className="menu-text">Contatti</div>
+                    </Link>
+                </div>
+
+                <div className="menu-link">
+                    <Link to="/menu" onClick={() => props.toggleMobileMenu()}>
+                        <div className="menu-text">Menu</div>
+                    </Link>
+                </div>
+            </div>
+        </Col>
     </>;
 }
 
