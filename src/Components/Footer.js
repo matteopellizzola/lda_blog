7607f00a729d21a7acda5afaed6d562e57569646
matteopellizzola <57703690+matteopellizzola@users.noreply.github.com>
2022-12-27 20:26:35 +0100

    First Footer Integration

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,25 @@ function Footer (props) {
     return <>
         <Row id="footer">
             <Col>
-                Footer
+                <div>
+                    <h6 className="title">
+                        Laboratorio di Antonella
+                    </h6>
+                    <div className="address">
+                        via Ciccio cicci 32 (TO) <br/>
+                        Rivoli 10098
+                    </div>
+                    <div className="phone">
+                        +39 3213243543
+                    </div>
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    <h6 className="follow">
+                        Seguimi su: <a href="instagram.com/laboratoriodiantonella/">Instagram</a>
+                    </h6>
+                </div>
             </Col>
         </Row>
     </>;
