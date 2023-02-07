a048a507d60bac7c57855d0311f9bd8888abdb7c
matteopellizzola <s238334@studenti.polito.it>
2023-02-07 14:41:17 +0100

    Instafeed in About

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -1,3 +1,4 @@
+import InstaFeed from "../../Components/InstaFeedSwiper";
 import loadBiography from "../../database/loadBiography";
 
 import "./about.scss";
@@ -5,6 +6,8 @@ import "./about.scss";
 const biographyData = loadBiography();
 
 console.log(JSON.stringify(biographyData));
+
+const ig_token = 'IGQVJYdXVuWFpGci1VbXlGcXZAwR3JLbGd0Wi1FSXd2cmZAOT0J3djVnMzUwbEFCNUpXLS1vcVM3ZA3cyT3cyd1ZAlZAXU4SVJtVFdUdmJuTF8wc2VvMnh2YlUwQnoxZAmZA1cy1mdTZAEV1VLSk85dlhsTjlMWgZDZD';
 
 function About (props) {
     return <>
@@ -16,6 +19,10 @@ function About (props) {
             <p className="bio-text">
                 {biographyData.biographyObj.mainParagraph}
             </p>
+        </div>
+
+        <div className="instagram-container">
+            <InstaFeed token={ig_token} counter="12" />
         </div>
     </>;
 }
