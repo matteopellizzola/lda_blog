7debc96d1573ebf81579241b430b10187397b8fa
matteopellizzola <57703690+matteopellizzola@users.noreply.github.com>
2023-02-19 11:58:20 +0100

    Products - first style

--- src/Pages/Products/ProductTile.js
+++ src/Pages/Products/ProductTile.js
@@ -1,3 +1,5 @@
+import "./products.scss";
+
 function ProductTile (props) {
     console.log(JSON.stringify(props));
     const product = props.product;
@@ -8,23 +10,25 @@ function ProductTile (props) {
                     <img src="https://picsum.photos/1800/1440" alt='' className='' />
                 </div>
                 <div className="description-container">
-                    <h5 classNameName="name">{product.name}</h5>
+                    <h5 className="name">{product.name}</h5>
                     <div className="description">{product.description ? product.description : ''}</div>
 
-                    <div className="">
-                        <div>Ingredienti</div>
+                    <div className="ingredients">
+                        <div className="header">Ingredienti</div>
                         <div>{product.ingredients}</div>
                     </div>
 
                     <div className="product-footer">
-                        <div className="">
-                            {product.bakingDay ? <div><span className="">Sfornato il:</span> {product.bakingDay} </div> : ''}
-                        </div>
-                        <div className="">
-                            {product.shelfLife ? <div><span className="">Conservazione:</span> {product.shelfLife} </div> : ''}
+                        <div className="left-footer">
+                            <div className="">
+                                {product.bakingDay ? <div><span className="">Sfornato il:</span> {product.bakingDay} </div> : ''}
+                            </div>
+                            <div className="">
+                                {product.shelfLife ? <div><span className="">Conservazione:</span> {product.shelfLife} </div> : ''}
+                            </div>
                         </div>
 
-                        <div className="">
+                        <div className="right-footer">
                             {product.typeOfCooking ? <div><span className="">Cottura:</span> {product.typeOfCooking} </div> : ''}
                         </div>
                     </div>
