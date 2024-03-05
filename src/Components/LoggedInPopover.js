5fd28f3a856b24a0dc508d36becb7f185f7337aa
Matteo Pellizzola <m.pellizzola@reply.it>
2024-03-05 14:11:01 +0100

    Aggiunta bottone logout

--- src/Components/LoggedInPopover.js
+++ src/Components/LoggedInPopover.js
@@ -1,14 +1,20 @@
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
+import api from '../services/api';
 
 
-function LoggedinPopOver ({ userData }) {
+function LoggedinPopOver({ userData }) {
     const popover = (
         <Popover id="popover-user" className='popover'>
             <div className='popover-link-container'>
                 <a className='btn-link btn' href={userData.editPath}>Add a product</a>
                 <a className='btn-link btn' href={userData.mediaLibraryLink} target='_blank'>Image Library</a>
+                <div className='btn-link btn' onClick={() => {
+                    api.users.logout().then(data => {
+                        window.location.replace('/');
+                    });
+                }}>Logout</div>
             </div>
         </Popover>
     );
