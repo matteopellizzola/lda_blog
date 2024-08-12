22a64767f5ed9364ff714735e0df4627e68a5aaa
Matteo Pellizzola <m.pellizzola@reply.it>
2024-08-12 22:23:56 +0200

    Contents - About - initial commit

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -10,14 +10,31 @@ const ig_token = import.meta.env.VITE_REACT_APP_API_INSTAGRAM_TOKEN;
 function About(props) {
   return (
     <div className="padding-logo-top px-3">
-      <h1 className="text-center">Chi sono</h1>
+      <h1 className="text-center font-bigger">Chi sono</h1>
       <div className="about-wrapper">
         <img
-          className="image-bio"
+          className="image-bio px-1"
           src={biographyData.biographyObj.mainImage}
           alt="image_bio"
         />
-        <p className="bio-text">{biographyData.biographyObj.mainParagraph}</p>
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: biographyData.biographyObj.mainParagraph,
+          }}
+        ></p>
+
+        <div className="py-5">
+          <h2 className="text-center claim">Benvenuti nel mio laboratorio</h2>
+          <h4 className="text-center">dove c'è profumo di buono</h4>
+        </div>
+
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: biographyData.biographyObj.secondParagraph,
+          }}
+        ></p>
       </div>
 
       <div className="instagram-container">
