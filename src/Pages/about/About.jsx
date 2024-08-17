9e9fb381ba88c6954b0af6545cb699689d9ba310
Matteo Pellizzola <m.pellizzola@reply.it>
2024-08-17 14:39:20 +0200

    updateApp - scheda About con i18n

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import InstaFeed from "../../Components/InstaFeedSwiper";
 import loadBiography from "../../database/loadBiography";
 
@@ -8,9 +9,11 @@ const biographyData = loadBiography();
 const ig_token = import.meta.env.VITE_REACT_APP_API_INSTAGRAM_TOKEN;
 
 function About(props) {
+  const { t } = useTranslation();
+
   return (
     <div className="padding-logo-top px-3">
-      <h1 className="text-center font-bigger">Chi sono</h1>
+      <h1 className="text-center font-bigger">{t("about.title")}</h1>
       <div className="about-wrapper">
         <img
           className="image-bio px-1"
@@ -20,19 +23,47 @@ function About(props) {
         <p
           className="bio-text"
           dangerouslySetInnerHTML={{
-            __html: biographyData.biographyObj.mainParagraph,
+            __html: t("about.description.first"), //biographyData.biographyObj.mainParagraph,
+          }}
+        ></p>
+
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: t("about.description.second"),
+          }}
+        ></p>
+
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: t("about.description.third"),
+          }}
+        ></p>
+
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: t("about.description.fourth"),
           }}
         ></p>
 
         <div className="py-5">
-          <h2 className="text-center claim">Benvenuti nel mio laboratorio</h2>
-          <h4 className="text-center">dove c'è profumo di buono</h4>
+          <h2 className="text-center claim">{t("about.claim.first")}</h2>
+          <h4 className="text-center">{t("about.claim.second")}</h4>
         </div>
 
         <p
           className="bio-text"
           dangerouslySetInnerHTML={{
-            __html: biographyData.biographyObj.secondParagraph,
+            __html: t("about.description.fifth"),
+          }}
+        ></p>
+
+        <p
+          className="bio-text"
+          dangerouslySetInnerHTML={{
+            __html: t("about.description.sixth"),
           }}
         ></p>
       </div>
