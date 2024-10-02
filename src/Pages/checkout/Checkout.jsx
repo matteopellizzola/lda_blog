6a4b0dd14d24d16ca7187372f661a588dc0ded5f
Matteo Pellizzola <m.pellizzola@reply.it>
2024-10-02 22:05:31 +0200

    prime modifiche parametri form

--- src/Pages/checkout/Checkout.jsx
+++ src/Pages/checkout/Checkout.jsx
@@ -63,9 +63,9 @@ export const Checkout = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="from_name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="from_email" />
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
